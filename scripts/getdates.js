01c5b9f7583bc50d503088472721982044d6247c
izelomabarak <joseluisim.lds@gmail.com>
2025-05-10 19:45:49 -0600

    W01 Assignment: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+const year = document.querySelector("#year");
+
+const today = new Date();
+
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span> 🌮 Luis Izelo 🌮 Puebla, México`;
+
+const modified = document.querySelector("#modified");
+
+let lastModified = new Date(document.lastModified);
+
+modified.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat("en-US",{dateStyle: "full"}).format(lastModified)}</span>`;
