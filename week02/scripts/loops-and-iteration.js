12e94f0f7e0ce05525bae87415aa815287455566
izelomabarak <joseluisim.lds@gmail.com>
2025-05-19 13:52:12 -0600

    W02 Learning Activities

---
+++ week02/scripts/loops-and-iteration.js
@@ -0,0 +1,29 @@
+const DAYS = 6;
+const LIMIT = 30;
+let studentReport = [11, 42, 33, 64, 29, 37, 44];
+//for lopp
+for (let i = 0; i < studentReport.length; i++) {
+    if(studentReport[i] < LIMIT) {
+    console.long(studentReport[i]);
+    }
+}
+//while lopp
+let i = 0;
+while (i < studentReport.length) {
+    if (studentReport[i] < LIMIT) {
+        console.log(studentReport[i]);
+    }
+    i++;
+}
+// forEach loop
+studentReport.forEach(function (item) {
+    if (item < LIMIT) {
+        console.log(item);
+    }
+});
+// for...in loop
+for (let i in studentReport) {
+    if (studentReport[i] < LIMIT) {
+        console.log(studentReport[i]);
+    }
+}
